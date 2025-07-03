047eb00d7815390ed9c5c899a5e280172deb4526
Eray Bahar <eraybahar24@gmail.com>
2025-07-03 09:51:13 +0300

    Updated scraper setup: main.py + item list loading

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from core.scraper import get_listing_price
 import json
-name_input = input("Please enter the item name:")
+item_list = []
+with open("item_list.txt","r",encoding="utf-8") as f:
+    data = f.readlines()
+for line in data:
+    item_list.append(line.strip())
 with open("cs2.json","r",encoding="utf-8") as file:
     item_map = json.load(file)
-item_nameid = item_map.get(name_input)
-result = get_listing_price(item_nameid)
-if result["success"]:
-    raw_lowest_sell = (result["lowest_sell_order"])/100
-    raw_highest_buy = (result["highest_buy_order"])/100
-    formatted_lowest_sell = f"{raw_lowest_sell:,.2f}"
-    formatted_highest_buy = f"{raw_highest_buy:,.2f}"
-    print("🔻 Lowest Sell:", formatted_lowest_sell)
-    print("🔺 Highest Buy:", formatted_highest_buy)
+for item in item_list:
+
+    item_nameid = item_map.get(item)
+    result = get_listing_price(item_nameid)
+    if result["success"]:
+        raw_lowest_sell = (result["lowest_sell_order"])/100
+        raw_highest_buy = (result["highest_buy_order"])/100
+        formatted_lowest_sell = f"{raw_lowest_sell:,.2f}"
+        formatted_highest_buy = f"{raw_highest_buy:,.2f}"
+        print(f"{item}: ")
+        print("🔻 Lowest Sell:", formatted_lowest_sell)
+        print("🔺 Highest Buy:", formatted_highest_buy)
+    else:
+        print(f"{item} couldn't be found.")
     
     
