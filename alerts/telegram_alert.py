b2d9f849c103e20bea1bfa135f045f739e0510d1
Eray Bahar <eraybahar24@gmail.com>
2025-07-03 19:28:27 +0300

    Add Telegram alert for buy opportunity + .env config

--- alerts/telegram_alert.py
+++ alerts/telegram_alert.py
@@ -8,4 +8,4 @@ def send_telegram_alert(message, token, chat_id):
     }
     response = requests.post(url,data=data)
     if response.status_code !=200:
-        print("❌ Telegram alert failed.")
+        print("❌ Telegram alert failed")
