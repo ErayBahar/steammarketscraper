c7a104f6c12c7d36adba935d47ef71d8f44619d9
Eray Bahar <eraybahar24@gmail.com>
2025-07-04 00:25:10 +0300

    Add timer-based loop and Telegram Alerts

--- main.py
+++ main.py
@@ -3,41 +3,49 @@ from alerts.telegram_alert import send_telegram_alert
 from dotenv import load_dotenv
 import os
 import json
+import time
 item_list = []
+def check_prices():
+    with open("item_list.txt","r",encoding="utf-8") as f:
+        data = f.readlines()
+    for line in data:
+        item_list.append(line.strip())
+    with open("cs2.json","r",encoding="utf-8") as file:
+        item_map = json.load(file)
+    for item in item_list:
+
+        item_nameid = item_map.get(item)
+        result = get_listing_price(item_nameid)
+        if result["success"]:
+            raw_lowest_sell = (result["lowest_sell_order"])/100
+            raw_highest_buy = (result["highest_buy_order"])/100
+            formatted_lowest_sell = f"{raw_lowest_sell:,.2f}"
+            formatted_highest_buy = f"{raw_highest_buy:,.2f}"
+            print(f"{item}: ")
+            print("🔻 Lowest Sell:", formatted_lowest_sell)
+            print("🔺 Highest Buy:", formatted_highest_buy)
+            if raw_lowest_sell < raw_highest_buy:
+                print("🟢 BUY OPPORTUNITY! Seller is undercutting highest bid.")
+
+                alert_msg = (
+                    f"🟢 BUY ALERT!\n"
+                    f"{item}\n"
+                    f"🔻 Lowest Sell: {formatted_lowest_sell} USD\n"
+                    f"🔺 Highest Buy: {formatted_highest_buy} USD\n"
+                    f"https://steamcommunity.com/market/listings/730/{item_nameid}"
+                )
+                send_telegram_alert(alert_msg,TELEGRAM_BOT_TOKEN,CHAT_ID)
+
+        else:
+            print(f"{item} couldn't be found.")
+        
 load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-with open("item_list.txt","r",encoding="utf-8") as f:
-    data = f.readlines()
-for line in data:
-    item_list.append(line.strip())
-with open("cs2.json","r",encoding="utf-8") as file:
-    item_map = json.load(file)
-for item in item_list:
-
-    item_nameid = item_map.get(item)
-    result = get_listing_price(item_nameid)
-    if result["success"]:
-        raw_lowest_sell = (result["lowest_sell_order"])/100
-        raw_highest_buy = (result["highest_buy_order"])/100
-        formatted_lowest_sell = f"{raw_lowest_sell:,.2f}"
-        formatted_highest_buy = f"{raw_highest_buy:,.2f}"
-        print(f"{item}: ")
-        print("🔻 Lowest Sell:", formatted_lowest_sell)
-        print("🔺 Highest Buy:", formatted_highest_buy)
-        if raw_lowest_sell < raw_highest_buy:
-            print("🟢 BUY OPPORTUNITY! Seller is undercutting highest bid.")
-
-            alert_msg = (
-                f"🟢 BUY ALERT!\n"
-                f"{item}\n"
-                f"🔻 Lowest Sell: {formatted_lowest_sell} USD\n"
-                f"🔺 Highest Buy: {formatted_highest_buy} USD\n"
-                f"https://steamcommunity.com/market/listings/730/{item_nameid}"
-            )
-            send_telegram_alert(alert_msg,TELEGRAM_BOT_TOKEN,CHAT_ID)
-
-    else:
-        print(f"{item} couldn't be found.")
-    
+if __name__ == "__main__":
+    while True:
+        print("🔄 Checking Steam Market prices...\n")
+        check_prices()
+        print("\n⏳ Waiting 60 seconds before next check...\n")
+        time.sleep(60)
     
